008cf6e694f8c448837bbb14e9bab73bac3b3c06
MYCOMPUTER\tsumu <tnks0920@gmail.com>
2023-10-28 02:41:34 +0900

    10/28 辻野 : マップ基礎追加

--- Assets/script/enemymove.cs
+++ Assets/script/enemymove.cs
@@ -10,39 +10,184 @@ public class enemymove : MonoBehaviour
 	public float speed = 2; // スピード：Inspectorで指定
 
 	public bool playerTurn;
+	public bool up = true;
+	public bool down = true;
+	public bool right = true;
+	public bool left = true;
+	private Vector3 latestPos;  //前回のPosition
+
+	public float PreX;
+	public float PreZ;
 
 	void Start()
 	{
-
+	
 	}
 
 	IEnumerator EMoveCoroutine()
 	{
 		//ここに処理を書く
-		Debug.Log("敵がうごいてる!");
+		//Debug.Log("敵がうごいてる!");
 
 		//1フレーム停止
-		yield return new WaitForSeconds(1.0f);
+		yield return new WaitForSeconds(1.5f);
 
 		//ここに再開後の処理を書く
+		transform.position = new Vector3(Mathf.Floor(this.transform.position.x), 0, Mathf.Floor(this.transform.position.z));
 		moveturn.playerTurn = true;
+		PreX = this.transform.position.x;
+		PreZ = this.transform.position.z;
 	}
+
+	IEnumerator ManyEnemyCoroutine()
+	{
+		//ここに処理を書く
+		//Debug.Log("敵がうごいてる!");
+
+		//1フレーム停止
+		yield return new WaitForSeconds(0.1f);
+
+		//ここに再開後の処理を書く
+		transform.position = new Vector3(Mathf.Floor(this.transform.position.x), 0, Mathf.Floor(this.transform.position.z));
+		moveturn.playerTurn = true;
+		PreX = this.transform.position.x;
+		PreZ = this.transform.position.z;
+	}
+
+	/*public void Reposition()
+    {
+		Debug.Log("敵がうごいてる!");
+		ENx / Mathf.Floor = PreX;
+		moveturn.playerTurn = true;
+		PreX = this.transform.position.x;
+
+	}*/
 
 	void Update()
 	{ // ずっと行う
-		player = GameObject.Find("Kamereon"); //Updataで使うのでオブジェクト読み込み
+		player = GameObject.Find("player"); //Updataで使うのでオブジェクト読み込み
 		moveturn = player.GetComponent<moveturn>(); //同様にスクリプトも読み込み
 
-		Vector3 pos = transform.position;
-		
+		Transform ENtransform = this.transform;//transformを取得
+		Vector3 pos = ENtransform.position;
+		Vector3 diff = transform.position - latestPos;   //前回からどこに進んだかをベクトルで取得
+		latestPos = transform.position;  //前回のPositionの更新
+		ENtransform.position = pos;  // 座標を設定
+
+		var PLx = player.transform.position.x;
+		var PLz = player.transform.position.z;
+		var ENx = this.transform.position.x;
+		var ENz = this.transform.position.z;
+
 		if (moveturn.playerTurn == false)
 		{
-			Debug.Log("kitayo");
-			transform.position += new Vector3(1, 0, 0) * Time.deltaTime;
-			StartCoroutine("EMoveCoroutine");
-			//moveturn.playerTurn = true;
+			//Debug.Log(moveturn.transform.position + "a");
+			//Debug.Log("(" + PLx + "." + PLz + ")" + " " + "(" + ENx + "." + ENz + ")");
+			//ENtransform.position += new Vector3(1, 0, 0) * Time.deltaTime;
+			//StartCoroutine("EMoveCoroutine");
+			if (Mathf.Floor(moveturn.PPreX) < Mathf.Floor(ENx) && left == true)//左
+			{
+				ENtransform.position += new Vector3(-1, 0, 0) * Time.deltaTime;
+				//Debug.Log(ENx + " " + PreX);
+				StartCoroutine("EMoveCoroutine");
+				up = false;
+				down = false;
+				right = false;
 
+			}
+			else if (Mathf.Floor(moveturn.PPreX) > Mathf.Floor(ENx) && right == true)//右
+			{
+				ENtransform.position += new Vector3(1, 0, 0) * Time.deltaTime;
+				//Debug.Log(ENx + " " + PreX);
+				StartCoroutine("EMoveCoroutine");
+				up = false;
+				down = false;
+				left = false;
+			}
+			else if (Mathf.Floor(moveturn.PPreX) == Mathf.Floor(PreX))
+			{
+				if (moveturn.PPreZ < ENz && down == true)//下
+				{
+					ENtransform.position += new Vector3(0, 0, -1) * Time.deltaTime;
+					Debug.Log(ENz + " " + PreZ);
+					StartCoroutine("EMoveCoroutine");
+					up = false;
+					right = false;
+					left = false;
+				}
+				else if (moveturn.PPreZ > ENz && up == true)//上
+				{
+					ENtransform.position += new Vector3(0, 0, 1) * Time.deltaTime;
+					//Debug.Log(ENz + " " + PreZ);
+					StartCoroutine("EMoveCoroutine");
+					down = false;
+					right = false;
+					left = false;
+				}
+			}
+
+			if (Mathf.Floor(Mathf.Abs(PreX)) > Mathf.Floor(Mathf.Abs(ENx)))
+			{
+				if (Mathf.Abs(PreX) - Mathf.Abs(ENx) > 0.9)
+				{
+					Debug.Log("a");
+					moveturn.playerTurn = true;
+					//StartCoroutine("ManyEnemyCoroutine");
+					PreX = this.transform.position.x;
+				}
+			}
+			else if (Mathf.Floor(Mathf.Abs(PreX)) < Mathf.Floor(Mathf.Abs(ENx)))
+			{
+				if (Mathf.Abs(ENx) - Mathf.Abs(PreX) > 1.15)
+				{
+					Debug.Log("a");
+					moveturn.playerTurn = true;
+					//StartCoroutine("ManyEnemyCoroutine");
+					PreX = this.transform.position.x;
+				}
+			}
+			else if (Mathf.Floor(moveturn.PPreX) == Mathf.Floor(PreX))
+			{
+				if (PreZ > ENz)
+				{
+					if (PreZ - ENz > 0.8)
+					{
+						Debug.Log("a");
+						moveturn.playerTurn = true;
+						//StartCoroutine("ManyEnemyCoroutine");
+						PreZ = this.transform.position.z;
+					}
+				}
+				else if (PreZ < ENz)
+				{
+					if (ENz - PreZ > 1)
+					{
+						Debug.Log("a");
+						moveturn.playerTurn = true;
+						//StartCoroutine("ManyEnemyCoroutine");
+						PreZ = this.transform.position.z;
+					}
+				}
+			}
 		}
+		else if (moveturn.playerTurn == true)
+		{
+			StopCoroutine("EMoveCoroutine");
+			transform.position = new Vector3(Mathf.Floor(PreX), 0, Mathf.Floor(PreZ));
+			moveturn.playerTurn = true;
+			//PreX = this.transform.position.x;
+			//PreZ = this.transform.position.z;
+			up = true;
+			down = true;
+			right = true;
+			left = true;
+		}
+
+		if (diff.magnitude > 0.01f)
+		{
+			transform.rotation = Quaternion.LookRotation(diff); //向きを変更する
+		}
+
 	}
 	void FixedUpdate()
 	{ // ずっと行う（一定時間ごとに）
--- Assets/script/moveturn.cs
+++ Assets/script/moveturn.cs
@@ -5,16 +5,29 @@ using UnityEngine;
 // キーを押すと、スプライトが移動する
 public class moveturn : MonoBehaviour
 {
+	GameObject GameManager;//Gamemanager読み込み
+	GameManager gameManager;
+
 	public GameObject enemy; //オブジェクト読み込み
 	public enemymove enemymove;
 	public float speed = 2; // スピード：Inspectorで指定
 
 	public bool playerTurn;
 
+	public float PPreX;
+	public float PPreZ;
+	public bool up = true;
+	public bool down = true;
+	public bool right = true;
+	public bool left = true;
+	private bool isNormal = false; //通常の床にいるかどうかのBool
+	private Vector3 latestPos;  //前回のPosition
+
 	void Start()
 	{
 		playerTurn = true;
-
+		GameManager = GameObject.Find("GameManager");
+		gameManager = GameManager.GetComponent<GameManager>(); // スクリプトを取得
 	}
 
 	public void PauseGame()
@@ -27,7 +40,7 @@ public class moveturn : MonoBehaviour
 		Time.timeScale = 1;
 	}
 
-	IEnumerator MoveCoroutine()
+	/*IEnumerator MoveCoroutine()
 	{
 		//ここに処理を書く
 		Debug.Log("うごいてる!");
@@ -37,40 +50,177 @@ public class moveturn : MonoBehaviour
 
 		//ここに再開後の処理を書く
 		playerTurn = false;
+	}*/
+
+	IEnumerator ZahyouCoroutine()
+	{
+		//ここに処理を書く
+		Debug.Log("修正中");
+
+		//1フレーム停止
+		yield return new WaitForSeconds(0.0f);
+
+		//ここに再開後の処理を書く
+		Debug.Log("完了");
+		Debug.Log(Mathf.Floor(PPreX) + " " + this.transform.position.y + " " + Mathf.Floor(PPreZ));
+		transform.position = new Vector3(Mathf.Floor(PPreX), this.transform.position.y, Mathf.Floor(PPreZ));
+		//gameManager.CallInoperable(2.0f); // 2 秒間　このスクリプトを無効に
+	}
+
+		void OnTriggerEnter(Collider other) //通常床のゾーンに入った際の処理
+	{
+		// タグを比較
+		if (other.gameObject.tag == "Normal")
+		{
+			// セーフフラグを外す
+			isNormal = true;
+			Debug.Log("Enter SafeZone");
+		}
+	}
+	private void OnTriggerStay(Collider other)
+	{
+		if (other.gameObject.name == "Yellow")
+		{
+			GetComponent<Renderer>().material.color = Color.yellow;
+			isNormal = true;
+			Debug.Log("Enter SafeZone");
+		}
+		else if (other.gameObject.name == "Red")
+        {
+			GetComponent<Renderer>().material.color = Color.red;
+			isNormal = true;
+			Debug.Log("Enter SafeZone");
+		}
+		else if (other.gameObject.name == "Blue")
+		{
+			GetComponent<Renderer>().material.color = Color.blue;
+			isNormal = true;
+			Debug.Log("Enter SafeZone");
+		}
 	}
 
 	void Update()
 	{ // ずっと行う
-		Vector3 pos = transform.position;
+		Transform PLtransform = this.transform;//transformを取得
+		Vector3 pos = PLtransform.position;
+		Vector3 diff = transform.position - latestPos;   //前回からどこに進んだかをベクトルで取得
+		latestPos = transform.position;  //前回のPositionの更新
+		var PLx = this.transform.position.x;
+		var PLz = this.transform.position.z;
+		PLtransform.position = pos;  // 座標を設定
+                                     //Vector3 posi = this.transform.position;//ローカル座標(moveturnがアタッチされているオブジェクトの座標)を取得
 
+        if (isNormal)
+        {
+			//Debug.Log("");
+		}
+		
 		if (playerTurn == true)
 		{
-			if (Input.GetKey("d"))
+			if (Input.GetKey("d") && right == true)
 			{ // もし、右キーが押されたら
-				transform.position += new Vector3(1, 0, 0) * Time.deltaTime;
-				Debug.Log("ikuo");
-				StartCoroutine("MoveCoroutine");
+				PLtransform.position += new Vector3(1, 0, 0) * Time.deltaTime;
+				//Debug.Log(PLx + " " + PPreX);
+				//StartCoroutine("MoveCoroutine");
+				up = false;
+				down = false;
+				left = false;
 			}
-			if (Input.GetKey("a"))
+			if (Input.GetKey("a") && left == true)
 			{ // もし、左キーが押されたら
-				transform.position += new Vector3(-1, 0, 0) * Time.deltaTime;
-				StartCoroutine("MoveCoroutine");
+				PLtransform.position += new Vector3(-1, 0, 0) * Time.deltaTime;
+				//Debug.Log(PLx + " " + PPreX);
+				//StartCoroutine("MoveCoroutine");
+				up = false;
+				down = false;
+				right = false;
 			}
-			if (Input.GetKey("w"))
+			if (Input.GetKey("w") && up == true)
 			{ // もし、上キーが押されたら
-				transform.position += new Vector3(0, 0, 1) * Time.deltaTime;
-				StartCoroutine("MoveCoroutine");
-				
+				PLtransform.position += new Vector3(0, 0, 1) * Time.deltaTime;
+				//StartCoroutine("MoveCoroutine");
+				//Debug.Log(PLz + " " + PPreZ);
+				down = false;
+				right = false;
+				left = false;
+
 			}
-			if (Input.GetKey("s"))
+			if (Input.GetKey("s") && down == true)
 			{ // もし、下キーが押されたら
-				transform.position += new Vector3(0, 0, -1) * Time.deltaTime;
-				StartCoroutine("MoveCoroutine");
+				PLtransform.position += new Vector3(0, 0, -1) * Time.deltaTime;
+				//StartCoroutine("MoveCoroutine");
+				//Debug.Log(PLz + " " + PPreZ);
+				up = false;
+				right = false;
+				left = false;
+			}
+
+			
+			if(Mathf.Abs(PPreX) > Mathf.Abs(PLx))//左行った後
+            {
+				if (Mathf.Abs(PPreX) - Mathf.Abs(PLx) > 0.9)
+				{
+					PPreX = PLx;
+					StartCoroutine("ZahyouCoroutine");
+					playerTurn = false;
+				}
+			}
+            else if (Mathf.Abs(PPreX) < Mathf.Abs(PLx))//右行った後
+			{
+				if (Mathf.Abs(PLx) - Mathf.Abs(PPreX) > 1)
+				{
+					PPreX = PLx;
+					StartCoroutine("ZahyouCoroutine");
+					playerTurn = false;
+				}
+			}
+
+			if (PPreZ > PLz)//下行った後
+			{
+				if (Mathf.Abs(PPreZ) - Mathf.Abs(PLz) > 0.9)
+				{
+					PPreZ = PLz;
+					StartCoroutine("ZahyouCoroutine");
+					playerTurn = false;
+					//PPreX = this.transform.position.x;
+					//PPreZ = this.transform.position.z;
+				}
+			}
+			else if (Mathf.Abs(PPreZ) < Mathf.Abs(PLz))//上行った後
+			{
+				if (Mathf.Abs(PLz) - Mathf.Abs(PPreZ) > 1)
+				{
+					PPreZ = PLz;
+					StartCoroutine("ZahyouCoroutine");
+					playerTurn = false;
+					//PPreX = this.transform.position.x;
+					//PPreZ = this.transform.position.z;
+				}
 			}
 
 		}
+		else if (playerTurn == false)
+        {
+			//PPreX = this.transform.position.x;
+			//PPreZ = this.transform.position.z;
+			//transform.position = new Vector3(Mathf.Floor(PLx), this.transform.position.y, Mathf.Floor(PLz));
+			Debug.Log("a" + Mathf.Floor(PLx) + " " + Mathf.Floor(PLz));
+			up = true;
+			down = true;
+			right = true;
+			left = true;
+			//gameManager.CallInoperable(0.5f); // 2 秒間　このスクリプトを無効に
+		}
 
-		
+		/*
+		//ベクトルの大きさが0.01以上の時に向きを変える処理をする
+		//＊＊＊挙動が変になるアクティブ禁止！！＊＊＊
+		if (diff.magnitude > 0.01f)
+		{
+			transform.rotation = Quaternion.LookRotation(diff); //向きを変更する
+		}
+		*/
+
 		if (Input.GetKey("escape"))
 		{ // もし、下キーが押されたら
 			Debug.Log("DON!");
