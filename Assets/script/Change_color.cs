dca747f0d6bb8c8ae5943996512dacbb49161fb6
YoshidaTowa <towaoyaji200364@gmail.com>
2023-11-07 14:39:56 +0900

    マップ修正 索敵範囲の透明化

--- Assets/script/Change_color.cs
+++ Assets/script/Change_color.cs
@@ -7,10 +7,15 @@ public class Change_color : MonoBehaviour
 	GameObject GameManager;//Gamemanager読み込み
 	GameManager gameManager;
 
-	public GameObject normal; //オブジェクト読み込み
-	public GameObject col; //オブジェクト読み込み
+    public GameObject node; //オブジェクト読み込み
 
-	void Start()
+    public Material Red;
+	public Material Blue;
+	public Material Yellow;
+	public Material Normal;
+
+
+    void Start()
 	{
 		GameManager = GameObject.Find("GameManager");
 		gameManager = GameManager.GetComponent<GameManager>(); // スクリプトを取得
@@ -20,22 +25,25 @@ public class Change_color : MonoBehaviour
 	{
 		if (other.gameObject.name == "Yellow_Change")
 		{
-			GetComponent<Renderer>().material.color = Color.yellow;
-			normal.GetComponent<Renderer>().material.color = new Color(0, 0, 0, 0);
-			col.GetComponent<Renderer>().material.color = new Color(255, 255, 0, 255);
+            node.GetComponent<Renderer>().material = Yellow;
+			gameObject.name = "Yellow_Player";
 		}
 		else if (other.gameObject.name == "Red_Change")
 		{
-			GetComponent<Renderer>().material.color = Color.red;
-			normal.GetComponent<Renderer>().material.color = new Color(0, 0, 0, 0);
-			col.GetComponent<Renderer>().material.color = new Color(255, 0, 0, 255);
+            node.GetComponent<Renderer>().material = Red;
+			gameObject.name = "Red_Player";
 		}
 		else if (other.gameObject.name == "Blue_Change")
 		{
-			GetComponent<Renderer>().material.color = Color.blue;
-			normal.GetComponent<Renderer>().material.color = new Color(0, 0, 0, 0);
-			col.GetComponent<Renderer>().material.color = new Color(0, 0, 255, 255);
+            node.GetComponent<Renderer>().material = Blue;
+			gameObject.name = "Blue_Player";
 		}
+		else if (other.gameObject.name == "Normal_Change")
+		{
+            node.GetComponent<Renderer>().material = Normal;
+            gameObject.name = "Normal_Player";
+
+        }
 	}
 }
 
