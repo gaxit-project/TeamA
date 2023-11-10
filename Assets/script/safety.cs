5c2cff05c71545dba8b0cc3637339cd7193a69eb
YoshidaTowa <towaoyaji200364@gmail.com>
2023-11-10 22:27:58 +0900

    色を変えるスクリプトと見つかる判定のスクリプトをMapYoshidaに追加

--- Assets/script/safety.cs
+++ Assets/script/safety.cs
@@ -4,15 +4,74 @@ using UnityEngine;
 
 public class safety : MonoBehaviour
 {
-    // Start is called before the first frame update
+    GameObject GameManager;//Gamemanager読み込み
+    GameManager gameManager;
+
+    public GameObject enemy; //オブジェクト読み込み
+    public enemymove enemymove;
+    public GameObject enemyhantei; //オブジェクト読み込み
+    public GameObject enemy2; //オブジェクト読み込み
+    public enemymove enemymove2;
+    public GameObject enemyhantei2; //オブジェクト読み込み
+    public GameObject enemy3; //オブジェクト読み込み
+    public enemymove enemymove3;
+    public GameObject enemyhantei3; //オブジェクト読み込み
+
     void Start()
     {
-        
+        GameManager = GameObject.Find("GameManager");
+        gameManager = GameManager.GetComponent<GameManager>(); // スクリプトを取得
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerStay(Collider other)
     {
-        
+        if (other.gameObject.name == "Yellow" && gameObject.name == "Yellow_Playerr") //黄色に触れて、かつ自分も黄色のとき
+        {
+            enemymove.sakuteki = false;
+            enemyhantei.SetActive(true);
+            enemymove2.sakuteki = false;
+            enemyhantei2.SetActive(true);
+            enemymove3.sakuteki = false;
+            enemyhantei3.SetActive(true);
+            //Debug.Log("Enter SafeZone");
+        }
+        else if (other.gameObject.name == "Red" && gameObject.name == "Red_Player") //赤色に触れて、かつ自分も赤色のとき
+        {
+            enemymove.sakuteki = false;
+            enemyhantei.SetActive(true);
+            enemymove2.sakuteki = false;
+            enemyhantei2.SetActive(true);
+            enemymove3.sakuteki = false;
+            enemyhantei3.SetActive(true);
+            //Debug.Log("Enter SafeZone");
+        }
+        else if (other.gameObject.name == "Blue" && gameObject.name == "Blue_Player") //青色に触れて、かつ自分も青色のとき
+        {
+            enemymove.sakuteki = false;
+            enemyhantei.SetActive(true);
+            enemymove2.sakuteki = false;
+            enemyhantei2.SetActive(true);
+            enemymove3.sakuteki = false;
+            enemyhantei3.SetActive(true);
+            //Debug.Log("Enter SafeZone");
+        }
+        else if (other.gameObject.name == "Hantei")
+        {
+            //Debug.Log("見つかった!");
+            enemymove.sakuteki = true;
+            enemyhantei.SetActive(false);   // 無効にする
+        }
+        else if (other.gameObject.name == "Hantei2")
+        {
+            //Debug.Log("見つかった!");
+            enemymove2.sakuteki = true;
+            enemyhantei2.SetActive(false);   // 無効にする
+        }
+        else if (other.gameObject.name == "Hantei3")
+        {
+            //Debug.Log("見つかった!");
+            enemymove3.sakuteki = true;
+            enemyhantei3.SetActive(false);   // 無効にする
+        }
     }
 }
