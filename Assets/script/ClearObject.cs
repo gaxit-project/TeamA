9031bbd9c03cbc5cdf24fc782f6e7ce202254197
KandaNaoki-1118 <naoki.kanda@mix-lab.net>
2023-11-15 15:24:15 +0900

    20231115 クリアオブジェクトに触れた瞬間に消えるスクリプトの実装。スクリプトはMapTutorial MapKanda MapYoshidaにアタッチしてます。　神田

---
+++ Assets/script/ClearObject.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ClearObject : MonoBehaviour
+{
+    public UnityEvent OnDestroyed = new UnityEvent();
+    //bool BeKey = true;
+
+    private void OnDestroy()
+    {
+        OnDestroyed.Invoke();
+        Debug.Log("クリアマスに入った！");
+        Destroy(gameObject, 0.0f);
+        /*if(BeKey == false)
+        {
+            OnDestroyed.Invoke();
+            //Destroy(gameObject, 0.2f);
+            Debug.Log("鍵ゲット！");
+        }*/
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            OnDestroy();
+            //bool BeKey = false;
+            //Debug.Log("検知！");
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
